8f67fa26b2ce52b250f13c1e76e614586ae61e65
Liam Hebblewhite <l.hebblewhite1@gmail.com>
2023-05-01 12:07:05 +0100

    Desktop static layout implemented.

--- index.js
+++
@@ -1,7 +0,0 @@
-const infoContainer = document.getElementsByClassName("infoContainer");
-
-menuElipsis.addEventListener("hover", event => {
-    for (let i = 0; i < infoContainer.length; i++){
-        infoContainer[i].style.filter = brightness("0%");
-    }
-})
